c4a8aa970a118f6b800ae1a912503145d3537d82
Frannellk Rojas <frannelk@gmail.com>
2025-06-23 13:10:34 -0400

    adding delete account

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -156,3 +156,10 @@ class TestAccountService(TestCase):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+     # TEST CASE FOR DELETING #
+    def test_delete_account(self):
+        """It should Delete an Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.delete(f"{BASE_URL}/{account.id}")
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
